1a61b45c0ed4f83e0890e65563884d7ec6b585fb
Laurkan Kushu <kanya384@mail.ru>
2024-05-25 22:45:21 +0300

    Code Style во всех файлах

--- src/task/TaskManager.java
+++ src/task/TaskManager.java
@@ -5,29 +5,42 @@ import java.util.List;
 
 public interface TaskManager {
     ArrayList<Task> getTasks();
+
     ArrayList<SubTask> getSubTasks();
+
     ArrayList<Epic> getEpics();
+
     ArrayList<SubTask> getEpicSubtasks(int epicId);
 
 
     void clearTasks();
+
     void clearSubTasks();
+
     void clearEpics();
 
     Task getTaskById(int id);
+
     SubTask getSubTaskById(int id);
+
     Epic getEpicById(int id);
 
     int createTask(Task task);
+
     int createSubTask(SubTask subTask);
+
     int createEpic(Epic epic);
 
     void updateTask(Task task);
+
     void updateSubTask(SubTask subTask);
+
     void updateEpic(Epic epic);
 
     void deleteTaskById(int id);
+
     void deleteSubTaskById(int id);
+
     void deleteEpicById(int epicId);
 
     List<Task> getHistory();
