e8759c633847d699cb43d219f60f673d982366b0
Laurkan Kushu <kanya384@mail.ru>
2024-05-25 22:38:00 +0300

    Правки Code Style

--- src/task/SubTask.java
+++ src/task/SubTask.java
@@ -2,6 +2,7 @@ package task;
 
 public class SubTask extends Task{
     private final int epicId;
+
     public SubTask(String name, String description, Status status, int epicId) {
         super(name, description, status);
         this.epicId = epicId;
