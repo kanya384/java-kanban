bbc11fa1db6db552a454846f790d3e4435a912d9
Laurkan Kushu <kanya384@mail.ru>
2024-05-26 16:14:19 +0300

    Поправил замечания

--- src/task/InMemoryHistoryManager.java
+++ src/task/InMemoryHistoryManager.java
@@ -3,11 +3,59 @@ package task;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Objects;
 
 public class InMemoryHistoryManager implements HistoryManager {
     private final HashMap<Integer, Node<Task>> tasks = new HashMap<>();
     private Node<Task> firstNode = null;
     private Node<Task> lastNode = null;
+
+    private class Node<T> {
+        private final T item;
+        private Node<T> next;
+        private Node<T> prev;
+
+        Node(Node<T> prev, T item, Node<T> next) {
+            this.prev = prev;
+            this.item = item;
+            this.next = next;
+        }
+
+
+        T getItem() {
+            return item;
+        }
+
+        Node<T> getNext() {
+            return next;
+        }
+
+        Node<T> getPrev() {
+            return prev;
+        }
+
+
+        void setNext(Node<T> next) {
+            this.next = next;
+        }
+
+        void setPrev(Node<T> prev) {
+            this.prev = prev;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Node<?> node = (Node<?>) o;
+            return Objects.equals(item, node.item) && Objects.equals(next, node.next) && Objects.equals(prev, node.prev);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(item, next, prev);
+        }
+    }
 
     @Override
     public void add(Task task) {
